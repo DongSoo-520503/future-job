f01fb5dc184f0f0627aac84fe43f95d639f8f408
DongSoo-520503 <kdss52@naver.com>
2026-03-01 20:24:49 +0900

    Update server port configuration and log message

--- app.js
+++ app.js
@@ -158,4 +158,7 @@ app.post('/recommend', (req, res) => {
     res.json({ text, buttons });
 });
 
-app.listen(3001, () => console.log('Server running on http://localhost:3001'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
